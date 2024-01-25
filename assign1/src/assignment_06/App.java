d4af673f8ef9051d7677a4d2b8aa2f39abbb090a
coulson-tu <156402192+coulson-tu@users.noreply.github.com>
2024-01-24 21:57:26 -0800

    modified the App.java

--- assign1/src/assignment_06/App.java
+++ assign1/src/assignment_06/App.java
@@ -5,37 +5,29 @@ import ignore.TestingUtils;
 public class App {
 
 	/**
-	We'll say a number is cool if it's a multiple of 11 or if it is one more than a multiple of 11. 
-	Return true if the given non-negative number is cool. Use the % "modulus" operator 
-	we spoke about in the prerequisite section. Watch the lesson on modulus if you need to review
-	<br>
-	<br>
-
+	 * We'll say a number is cool if it's a multiple of 11 or if it is one more than
+	 * a multiple of 11.
+	 * Return true if the given non-negative number is cool. Use the % "modulus"
+	 * operator
+	 * we spoke about in the prerequisite section. Watch the lesson on modulus if
+	 * you need to review
+	 * <br>
+	 * <br>
+	 * 
 	 * <b>EXPECTATIONS:</b><br>
-		isCool(22)   <b>---></b> true <br>
-		isCool(23)    <b>---></b> true <br>
-		isCool(24) <b>---></b> false <br>
+	 * isCool(22) <b>---></b> true <br>
+	 * isCool(23) <b>---></b> true <br>
+	 * isCool(24) <b>---></b> false <br>
 	 */
-		public static boolean isCool(int n) {
-			return true;
-		}
+	public static boolean isCool(int n) {
+		// Check if the number is a multiple of 11 or one more than a multiple of 11
+		return (n % 11 == 0) || (n % 11 == 1);
+	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	//----------------------STARTING POINT OF PROGRAM. IGNORE BELOW --------------------//
-	public static void main(String args[]){
+	// ----------------------STARTING POINT OF PROGRAM. IGNORE BELOW
+	// --------------------//
+	public static void main(String args[]) {
 		TestingUtils.runTests();
-		
+
 	}
 }
-
-
